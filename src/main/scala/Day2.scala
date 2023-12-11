3c5c8701f4c215517779a599bc3323a210ee66fe
Guillermo <guillermogarcialopez00@gmail.com>
2023-12-11 18:45:23 +0100

    Day2 part one completed

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -6,46 +6,19 @@ case class SetGame(reds: Int, blues: Int, greens: Int)
 object Day2 extends App with AdventUtils {
   val games: List[String] =  getDataOfDay(this.getClass.getSimpleName.replace("$", ""))
 
+  /* Part one */
   val numRedCubes: Int = 12
   val numGreenCubes: Int = 13
   val numBlueCubes: Int = 14
 
-  def getGameId(head: String): String = head.split(":").toList(0).split("Game ").toList.last
-  def trimGameHead(gameLine: String): String = gameLine.split(": ").toList.tail(0)
+  def getGameId(head: String): String = head.split(":").toList.head.split("Game ").toList.last
+  def trimGameHead(gameLine: String): String = gameLine.split(": ").toList.tail.head
 
   def processIndividualSet(setGame: List[String]): SetGame = {
-    SetGame(getReds(setGame), getBlues(setGame), getGreens(setGame))
+    SetGame(getColours("red", setGame), getColours("blue", setGame), getColours("green", setGame))
   }
-
-
-  //TODO Fusionar los tres métodos
-  def getReds(setGame: List[String]): Int = {
-    val numReds: Int = Try(setGame.filter(_.contains("red"))(0).filter(_.isDigit).toInt).getOrElse(0)
-    numReds
-  }
-
-  def getBlues(setGame: List[String]): Int = {
-    val numBlues: Int = Try(setGame.filter(_.contains("blue"))(0).filter(_.isDigit).toInt).getOrElse(0)
-    numBlues
-  }
-
-  def getGreens(setGame: List[String]): Int = {
-    val numGreens: Int = Try(setGame.filter(_.contains("green"))(0).filter(_.isDigit).toInt).getOrElse(0)
-    numGreens
-  }
-
-
-  def allSetsAreValid(listOfSets: List[SetGame]): Boolean = {
-    println("en allSetsAreValid con " + listOfSets)
-
-    val mapWithValids: List[Boolean] = listOfSets.map(s => redsValid(s) && bluesValid(s) && greensValid(s))
-
-    mapWithValids.foreach(println)
-
-    println("ergo results is :")
-    println(mapWithValids.forall(_ == true))
-
-    mapWithValids.forall(_ == true)
+  def getColours(colour: String, setGame: List[String]): Int = {
+    Try(setGame.filter(_.contains(colour)).head.filter(_.isDigit).toInt).getOrElse(0)
   }
 
   def redsValid(set: SetGame): Boolean = set.reds <= numRedCubes
@@ -56,22 +29,16 @@ object Day2 extends App with AdventUtils {
     val gameHead: String = getGameId(g)
     val setsFromGame: List[SetGame] = trimGameHead(g).split("; ").toList.map(_.split(", ").toList)
       .map(processIndividualSet)
-
     Map(gameHead -> setsFromGame)
   })
 
+  def allSetsAreValid(game: Map[String, List[SetGame]]): Map[String, Boolean] = {
+    val listOfSets: List[SetGame] = game.values.last
+    val mapWithValids: List[Boolean] = listOfSets.map(s => redsValid(s) && bluesValid(s) && greensValid(s))
+    Map(game.keys.last -> mapWithValids.forall(_ == true))
+  }
 
-  allSetsFromGame.foreach(println)
+  val validGames = allSetsFromGame.map(allSetsAreValid)
 
-
-  println("===========================================================================================================")
-  println("===========================================================================================================")
-
-
-  val validGames = allSetsFromGame.map(set => allSetsAreValid(set.values.last))
-
-  //validGames.foreach(println)
-
-
-
+  println(validGames.filter(_.values.last).map(_.keySet.head).map(_.toInt).sum)
 }
